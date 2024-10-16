85f67dd171dd80ce4a6bd9e2d411d9085b8edc3c
judemont <julien@rmbi.ch>
2024-10-16 15:38:57 +0200

    feat: Update newOrder.php to use POST method for receiving data
    The `newOrder.php` file has been modified to use the POST method instead of the GET method for receiving data. This change ensures that the `ids` and `quantities` parameters are sent as POST data. Additionally, error handling has been added to check if the count of `ids` matches the count of `quantities`. If the counts do not match or if the count of `ids` is less than 1, an error message is returned. The modified code also includes database operations to insert the order details and order products into the database.

--- api/newOrder.php
+++ api/newOrder.php
@@ -1,3 +1,34 @@
 <?php
 
-$ids = explode(",", $_GET["ids"]);
+$ids = explode(",", $_POST["ids"]);
+$quantities = explode(",", $_POST["quantities"]);
+
+if(count($ids) != count($quantities)) {
+    echo json_encode(array("error"=> "count(ids) != count(quantities)"));
+    exit();
+}
+if(count($ids) < 1) {
+    echo json_encode(array("error"=> "count(ids) < 1"));
+    exit();
+}
+$db = new Database;
+
+
+$name = $db-> escapeStrings($_POST["name"]);
+$email = $db-> escapeStrings($_POST["email"]);
+$phone = $db-> escapeStrings($_POST["phone"]);
+$address = $db-> escapeStrings($_POST["address"]);
+$postal_code = $db-> escapeStrings($_POST["postal_code"]);
+$city = $db-> escapeStrings($_POST["city"]);
+
+$db -> query("
+    INSERT INTO recytech_orders (name, email, phone, address, postal_code, city)
+    VALUES ('$name', '$email', '$phone', '$address', '$postal_code', '$city')");
+
+$orderId = $db->getLastInsertedID();
+
+for ($i=0; $i < count($ids); $i++) { 
+    $db -> query("
+        INSERT INTO recytech_order_products (order_ID, product_ID, quantity)
+        VALUES ('$orderId', '$ids[$i]', '$quantities[$i]')");
+}
